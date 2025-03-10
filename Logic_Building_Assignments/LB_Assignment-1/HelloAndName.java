e5b1c034b5d9fcc73f1a969462a78cc3719626d8
Mallikarjun Konchurkar <174715711+Mallikarjun-Konchurkar@users.noreply.github.com>
2025-03-10 19:08:37 +0530

    Delete Logic_Building_Assignments/LB_Assignment-1 directory

--- Logic_Building_Assignments/LB_Assignment-1/HelloAndName.java
+++
@@ -1,9 +0,0 @@
-//Write a Java program to print 'Hello' on the screen and then print your name on a separate line.
-class Hello
-{
-   public static void main(String args[])
-     {
-        System.out.println("Hello");
-	System.out.println("I am Mallikarjun Konchurkar"); 
-      }
-}
